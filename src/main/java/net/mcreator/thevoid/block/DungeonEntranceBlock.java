6c76d02f5e4d5897f9b864f6a70a88fd7a87770f
64038298+badgeminer@users.noreply.github.com <64038298+badgeminer@users.noreply.github.com>
2022-09-25 15:29:10 -0600

    e

--- src/main/java/net/mcreator/thevoid/block/DungeonEntranceBlock.java
+++ src/main/java/net/mcreator/thevoid/block/DungeonEntranceBlock.java
@@ -1,15 +1,30 @@
 
 package net.mcreator.thevoid.block;
 
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.thevoid.procedures.DungeonEntranceOnBlockRightClickedProcedure;
+
+import java.util.List;
+import java.util.Collections;
 
 public class DungeonEntranceBlock extends Block {
-
 	public DungeonEntranceBlock() {
 		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GRAVEL).strength(1f, 10f));
-
 	}
 
 	@Override
@@ -19,7 +34,6 @@ public class DungeonEntranceBlock extends Block {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
@@ -29,7 +43,6 @@ public class DungeonEntranceBlock extends Block {
 	@Override
 	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
 		super.use(blockstate, world, pos, entity, hand, hit);
-
 		int x = pos.getX();
 		int y = pos.getY();
 		int z = pos.getZ();
@@ -39,8 +52,6 @@ public class DungeonEntranceBlock extends Block {
 		Direction direction = hit.getDirection();
 
 		DungeonEntranceOnBlockRightClickedProcedure.execute(world, entity);
-
 		return InteractionResult.SUCCESS;
 	}
-
 }
--- src/main/java/net/mcreator/thevoid/world/dimension/TheDungeonDimension.java
+++ src/main/java/net/mcreator/thevoid/world/dimension/TheDungeonDimension.java
@@ -1,21 +1,38 @@
 
 package net.mcreator.thevoid.world.dimension;
 
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.levelgen.carver.WorldCarver;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import com.google.common.collect.ImmutableSet;
+
 @Mod.EventBusSubscriber
 public class TheDungeonDimension {
-
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class Fixers {
-
 		@SubscribeEvent
 		public static void registerFillerBlocks(FMLCommonSetupEvent event) {
 			Set<Block> replaceableBlocks = new HashSet<>();
 			replaceableBlocks.add(Blocks.AIR);
-
+			replaceableBlocks.add(Blocks.AIR);
+			replaceableBlocks.add(Blocks.AIR);
 			event.enqueueWork(() -> {
 				WorldCarver.CAVE.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CAVE.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
-
 				WorldCarver.CANYON.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CANYON.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
 			});
@@ -25,7 +42,6 @@ public class TheDungeonDimension {
 		@OnlyIn(Dist.CLIENT)
 		public static void registerDimensionSpecialEffects(FMLClientSetupEvent event) {
 			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(Float.NaN, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
-
 				@Override
 				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
 					return color;
@@ -35,12 +51,8 @@ public class TheDungeonDimension {
 				public boolean isFoggyAt(int x, int y) {
 					return false;
 				}
-
 			};
-
 			event.enqueueWork(() -> DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("thevoid:the_dungeon"), customEffect));
 		}
-
 	}
-
 }
