436368ea0cffe0b01379ed109eba7ffd470ff23f
105132084+randomstuffmod@users.noreply.github.com <steph@10.0.0.156>
2022-09-28 17:08:39 -0600

    hj

--- src/main/java/net/mcreator/thevoid/world/dimension/AtherdimDimension.java
+++ src/main/java/net/mcreator/thevoid/world/dimension/AtherdimDimension.java
@@ -1,24 +1,39 @@
 
 package net.mcreator.thevoid.world.dimension;
 
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.levelgen.carver.WorldCarver;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+import net.mcreator.thevoid.init.ThevoidModBlocks;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import com.google.common.collect.ImmutableSet;
+
 @Mod.EventBusSubscriber
 public class AtherdimDimension {
-
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class Fixers {
-
 		@SubscribeEvent
 		public static void registerFillerBlocks(FMLCommonSetupEvent event) {
 			Set<Block> replaceableBlocks = new HashSet<>();
 			replaceableBlocks.add(ThevoidModBlocks.ATHERGRASS.get());
-
 			replaceableBlocks.add(ThevoidModBlocks.ATHERGRASS.get());
 			replaceableBlocks.add(ThevoidModBlocks.ATHER_STONE.get());
-
 			event.enqueueWork(() -> {
 				WorldCarver.CAVE.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CAVE.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
-
 				WorldCarver.CANYON.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CANYON.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
 			});
@@ -28,7 +43,6 @@ public class AtherdimDimension {
 		@OnlyIn(Dist.CLIENT)
 		public static void registerDimensionSpecialEffects(FMLClientSetupEvent event) {
 			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(Float.NaN, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
-
 				@Override
 				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
 					return color;
@@ -38,12 +52,8 @@ public class AtherdimDimension {
 				public boolean isFoggyAt(int x, int y) {
 					return false;
 				}
-
 			};
-
 			event.enqueueWork(() -> DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("thevoid:atherdim"), customEffect));
 		}
-
 	}
-
 }
