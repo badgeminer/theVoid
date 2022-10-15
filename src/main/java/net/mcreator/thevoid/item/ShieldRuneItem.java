fb2f9fd17499b266359d326cc3b23d1a6238270b
105132084+randomstuffmod@users.noreply.github.com <steph@10.0.0.156>
2022-10-15 11:43:18 -0600

    fytr

--- src/main/java/net/mcreator/thevoid/item/ShieldRuneItem.java
+++ src/main/java/net/mcreator/thevoid/item/ShieldRuneItem.java
@@ -1,11 +1,13 @@
 
 package net.mcreator.thevoid.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 
 public class ShieldRuneItem extends Item {
-
 	public ShieldRuneItem() {
 		super(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.EPIC));
 	}
@@ -19,5 +21,4 @@ public class ShieldRuneItem extends Item {
 	public int getUseDuration(ItemStack itemstack) {
 		return 0;
 	}
-
 }
